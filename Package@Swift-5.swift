5db2accc18de04377a0380aac8c701fe9d066123
Helge Heß <me@helgehess.eu>
2019-05-12 14:50:16 +0200

    Use a specific Swift 5 Package.swift
    ... just for the tools version.

---
+++ Package@Swift-5.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+  name: "CApache",
+
+  products: [
+      .library(name: "CApache", targets: ["CApache"]),
+  ],
+  targets: [
+      .systemLibrary(name: "CApache",
+          pkgConfig: "mod_swift",
+          providers: [
+              .brew("modswift/mod_swift"),
+              .apt("apache2-dev") // Note: still requires mod_swift install!
+          ]
+      )
+  ]
+)
